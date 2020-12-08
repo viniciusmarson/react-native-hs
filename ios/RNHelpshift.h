94b288e029e501656d2a8dc1adea93dd16057bfc
Vinicius Marson <marson@MacBook-Air-de-Vinicius.local>
2020-12-08 13:05:25 -0300

    .

--- ios/RNHelpshift.h
+++ ios/RNHelpshift.h
@@ -1,7 +1,7 @@
+@import Helpshift
+
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
-
-@import Helpshift
 
 @interface RNHelpshift : RCTEventEmitter<RCTBridgeModule, HelpshiftSupportDelegate, HelpshiftInboxDelegate>
 
